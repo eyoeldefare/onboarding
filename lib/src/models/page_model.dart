1ef5fd617f030764b30f147e78d7b64c4e921d18
eyoeldefare <eyoeldefare@gmail.com>
2020-12-05 19:41:27 -0500

    page model properties are required and must not be null

--- lib/src/models/page_model.dart
+++ lib/src/models/page_model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/widgets.dart';
+
 class PageModel {
   ///[assetPath] is the path of the image you have somewhere inside your project
   ///[title] is the title of the page you provide
@@ -5,8 +7,10 @@ class PageModel {
   final String assetPath, title, info;
 
   PageModel({
-    this.assetPath,
-    this.title,
-    this.info,
-  });
+    @required this.assetPath,
+    @required this.title,
+    @required this.info,
+  })  : assert(assetPath != null),
+        assert(title != null),
+        assert(info != null);
 }
