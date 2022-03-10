0731cfbcf453752abfa01b05c83982c636b459f3
eyoeldefare <eyoeldefare@gmail.com>
2022-03-10 18:02:52 -0500

    add new example with new breaking changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,47 +11,102 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextButton _button = TextButton(
-    style: ButtonStyle(
-      foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-    ),
-    onPressed: () {},
-    child: Text('Skip'),
-  );
-
   final onboardingPagesList = [
     PageModel(
       widget: Column(
         children: [
-          Image.asset('assets/images/facebook.png', color: pageImageColor),
-          Text('SECURED BACKUP', style: pageTitleStyle),
-          Text(
-            'Keep your files in closed safe so you can\'t lose them',
-            style: pageInfoStyle,
-          )
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 45.0,
+              vertical: 90.0,
+            ),
+            child: Image.asset('assets/images/facebook.png',
+                color: pageImageColor),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 45.0),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'SECURED BACKUP',
+              style: pageTitleStyle,
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'Keep your files in closed safe so you can\'t lose them. Consider TrueNAS.',
+              style: pageInfoStyle,
+              textAlign: TextAlign.left,
+            ),
+          ),
         ],
       ),
     ),
     PageModel(
       widget: Column(
         children: [
-          Image.asset('assets/images/twitter.png', color: pageImageColor),
-          Text('CHANGE AND RISE', style: pageTitleStyle),
-          Text(
-            'Give others access to any file or folder you choose',
-            style: pageInfoStyle,
-          )
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 45.0,
+              vertical: 90.0,
+            ),
+            child:
+                Image.asset('assets/images/twitter.png', color: pageImageColor),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 45.0),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'CHANGE AND RISE',
+              style: pageTitleStyle,
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'Give others access to any file or folders you choose',
+              style: pageInfoStyle,
+              textAlign: TextAlign.left,
+            ),
+          ),
         ],
       ),
     ),
     PageModel(
       widget: Column(
         children: [
-          Image.asset('assets/images/instagram.png', color: pageImageColor),
-          Text('EASY ACCESS', style: pageTitleStyle),
-          Text(
-            'Reach your files anytime from any devices anywhere',
-            style: pageInfoStyle,
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 45.0,
+              vertical: 90.0,
+            ),
+            child: Image.asset('assets/images/instagram.png',
+                color: pageImageColor),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 45.0),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'EASY ACCESS',
+              style: pageTitleStyle,
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'Reach your files anytime from any devices anywhere',
+              style: pageInfoStyle,
+              textAlign: TextAlign.left,
+            ),
           ),
         ],
       ),
@@ -67,53 +122,55 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+
+      /**
+       * 
+       * 
+       * const defaultSkipButtonColor = Color.fromARGB(255, 93, 93, 93);
+        const defaultSkipButtonBorderRadius = BorderRadius.all(Radius.circular(20.0));
+const defaultSkipButtonPadding =
+    EdgeInsets.symmetric(horizontal: 17.0, vertical: 5.0);
+const defaultSkipButtonTextStyle =
+    TextStyle(color: Colors.white, letterSpacing: 1.0);
+
+//--------Proceed Button---------
+
+const defaultProceedButtonColor = Color.fromARGB(255, 88, 94, 147);
+const defaultProceedButtonBorderRadius =
+    BorderRadius.all(Radius.circular(20.0));
+const defaultProceedButtonPadding =
+    EdgeInsets.symmetric(horizontal: 17.0, vertical: 5.0);
+const defaultProceedButtonTextStyle = TextStyle(
+  color: Colors.white,
+  letterSpacing: 1.0,
+);
+       */
       home: Onboarding(
-        background: Colors.red,
-        onPageChange: (int page) {
-          if (page == 2) {
-            setState(() {
-              _button = TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () {},
-                child: Text('Procced'),
-              );
-            });
-          } else {
-            setState(() {
-              _button = TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () {},
-                child: Text('Skip'),
-              );
-            });
-          }
-        },
-        startPageIndex: 1,
         pages: onboardingPagesList,
         footer: Footer(
-          footerPadding: EdgeInsets.only(
-            bottom: 10.0,
-            left: 10.0,
-          ),
-          child: TextButton(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          child: Material(
+            borderRadius: defaultSkipButtonBorderRadius,
+            color: defaultSkipButtonColor,
+            child: InkWell(
+              borderRadius: defaultSkipButtonBorderRadius,
+              onTap: () {},
+              child: const Padding(
+                padding: defaultSkipButtonPadding,
+                child: Text(
+                  'Skip',
+                  style: defaultSkipButtonTextStyle,
+                ),
+              ),
             ),
-            onPressed: () {},
-            child: Text('TextButton'),
           ),
           indicator: Indicator(
-            indicatorDesign: IndicatorDesign.polygon(
-              polygonDesign: PolygonDesign(
-                  polygon: DesignType.polygon_square, polygonSpacer: 13.0),
+            indicatorDesign: IndicatorDesign.line(
+              lineDesign: LineDesign(
+                lineType: DesignType.line_uniform,
+              ),
             ),
           ),
+          footerPadding: const EdgeInsets.all(45.0),
         ),
       ),
     );
